99721081fca3e4afb34d21a9aa06ce8657cd9ac5
Sir-Floppa <nicolas.donoso.ge@outlook.com>
2022-10-20 17:35:40 -0300

    Terminadas las filas de Tabla Semanal y Categorias

--- app/src/main/java/com/example/produtracker2/MainActivity.kt
+++ app/src/main/java/com/example/produtracker2/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.produtracker2
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.tablerow.view.*
 import java.text.SimpleDateFormat
+import java.time.DayOfWeek
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,9 +30,27 @@ class MainActivity : AppCompatActivity() {
         btnCategoriasTabla.setOnClickListener{ cargarVista(categoriasIntent) }
         btnRegistroTabla.setOnClickListener{ cargarVista(registroIntent) }
 
+        if(!sqlManager.comprobarEliminacion(this) && LocalDate.now().dayOfWeek == DayOfWeek.MONDAY) {
+            sqlManager.anadirRegistro(this)
+            sqlManager.reiniciarFilas(this)
+            sqlManager.actualizarUso(this, 0)
+        }
+        else if(sqlManager.comprobarEliminacion(this) && LocalDate.now().dayOfWeek != DayOfWeek.MONDAY){
+            sqlManager.actualizarUso(this, 1)
+        }
+
         for(categoria in categorias) {
             try{
                 cargarFila(categoria, sqlManager.seleccionarFila(this, categoria.id))
+
+                var fila = sqlManager.seleccionarFila(this, categoria.id)
+                LSum.setText((LSum.text.toString().toInt() + fila.LValue * categoria.prioridad!!).toString())
+                MSum.setText((MSum.text.toString().toInt() + fila.MValue * categoria.prioridad!!).toString())
+                XSum.setText((XSum.text.toString().toInt() + fila.XValue * categoria.prioridad!!).toString())
+                JSum.setText((JSum.text.toString().toInt() + fila.JValue * categoria.prioridad!!).toString())
+                VSum.setText((VSum.text.toString().toInt() + fila.VValue * categoria.prioridad!!).toString())
+                SSum.setText((SSum.text.toString().toInt() + fila.SValue * categoria.prioridad!!).toString())
+                DSum.setText((DSum.text.toString().toInt() + fila.DValue * categoria.prioridad!!).toString())
             }
             catch(e: Exception) {
                 Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
--- app/src/main/java/com/example/produtracker2/SQLManager.kt
+++ app/src/main/java/com/example/produtracker2/SQLManager.kt
@@ -4,19 +4,131 @@ import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.os.Build
 import android.provider.BaseColumns
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.content.contentValuesOf
+import java.text.SimpleDateFormat
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.temporal.TemporalAdjuster
+import java.time.temporal.TemporalAdjusters
+import java.util.*
 import kotlin.reflect.typeOf
 
-class SQLManager(context: Context): SQLiteOpenHelper(context, "produtracker1.db", null, 1) {
+class SQLManager(context: Context): SQLiteOpenHelper(context, "produtracker4.db", null, 1) {
 
     override fun onCreate(db: SQLiteDatabase?) {
         db!!.execSQL("CREATE TABLE categorias (idCategoria INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nombre VARCHAR(24), prioridad INT)")
         db!!.execSQL("CREATE TABLE filasTablaSemanal (idFila INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, LValue	INTEGER DEFAULT 0, MValue INTEGER DEFAULT 0, XValue	INTEGER DEFAULT 0, JValue INTEGER DEFAULT 0, VValue	INTEGER DEFAULT 0, SValue INTEGER DEFAULT 0, DValue	INTEGER DEFAULT 0, idCategoria	INTEGER NOT NULL, FOREIGN KEY(idCategoria) REFERENCES categorias(idCategorias))")
+        db!!.execSQL("CREATE TABLE registros (idRegistro INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, fechaInicio TEXT, fechaFinal TEXT, valor INTEGER)")
+        db!!.execSQL("CREATE TABLE uso (eliminacionSemanal INTEGER DEFAULT 1)")
+        db!!.execSQL("INSERT INTO uso VALUES(1)")
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
         TODO("Not yet implemented")
+    }
+
+    // Uso
+    fun comprobarEliminacion(context: Context): Boolean {
+        var db = SQLManager(context)
+        var manager = db.writableDatabase
+
+        var response = true
+
+        var res = manager.query(
+            "uso",
+            arrayOf("eliminacionSemanal"),
+            null,
+            arrayOf(),
+            "",
+            null,
+            null
+        )
+
+        with(res) {
+            while(moveToNext()) {
+                response = getInt(getColumnIndexOrThrow("eliminacionSemanal")) == 1
+            }
+        }
+
+        return response
+    }
+
+    fun actualizarUso(context: Context, valor: Int) {
+        var db = SQLManager(context)
+        var manager = db.writableDatabase
+
+        var contentValues = ContentValues()
+
+        contentValues.put("eliminacionSemanal", valor)
+
+        manager.update(
+            "uso",
+            contentValues,
+            null,
+            null
+        )
+    }
+
+    // Registro
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun anadirRegistro(context: Context): Boolean {
+        var response = true
+
+        var db = SQLManager(context)
+        var manager = db.writableDatabase
+
+        val today = LocalDate.now()
+        var previousMonday = today.with(TemporalAdjusters.previous(DayOfWeek.MONDAY))
+        previousMonday = previousMonday.with(TemporalAdjusters.previous(DayOfWeek.MONDAY))
+
+        var previousSunday = today.with(TemporalAdjusters.previous(DayOfWeek.SUNDAY))
+
+        var categorias = leerCategorias(context)
+
+        var lsum = 0
+        var msum = 0
+        var xsum = 0
+        var jsum = 0
+        var vsum = 0
+        var ssum = 0
+        var dsum = 0
+        for (categoria in categorias) {
+            var fila = seleccionarFila(context, categoria.id)
+            lsum += fila.LValue * categoria.prioridad!!
+            msum += fila.LValue * categoria.prioridad!!
+            xsum += fila.LValue * categoria.prioridad!!
+            jsum += fila.LValue * categoria.prioridad!!
+            vsum += fila.LValue * categoria.prioridad!!
+            ssum += fila.LValue * categoria.prioridad!!
+            dsum += fila.LValue * categoria.prioridad!!
+        }
+
+        var total = lsum + msum + xsum + jsum + vsum + ssum + dsum
+
+        var contentValues = ContentValues()
+        contentValues.putNull("idRegistro")
+        contentValues.put("fechaInicio", previousMonday.toString())
+        contentValues.put("fechaFinal", previousSunday.toString())
+        contentValues.put("valor", total)
+
+        try {
+            manager.insert("registros", null, contentValues)
+        }
+        catch(e: Exception) {
+            print(e.message)
+            Toast.makeText(context, e.message, Toast.LENGTH_LONG).show()
+            response = false
+        }
+        finally {
+            db.close()
+        }
+
+        return response
     }
 
     // Filas
@@ -114,6 +226,22 @@ class SQLManager(context: Context): SQLiteOpenHelper(context, "produtracker1.db"
         }
 
         return res[0]
+    }
+
+    fun reiniciarFilas(context: Context) {
+        var db = SQLManager(context)
+        var manager = db.writableDatabase
+
+        var contentValues = ContentValues()
+        contentValues.put("LValue", 0)
+        contentValues.put("MValue", 0)
+        contentValues.put("XValue", 0)
+        contentValues.put("JValue", 0)
+        contentValues.put("VValue", 0)
+        contentValues.put("SValue", 0)
+        contentValues.put("DValue", 0)
+
+        manager.update("filasTablaSemanal", contentValues,null, null)
     }
 
     // Categorias
